8293b4ca64301cd3c9cc5eaaa9804610c56267b4
wesley.lacerda <wesley.lacerda@redeunifique.com.br>
2024-02-06 18:06:36 -0300

    first commit

---
+++ src/core/either.spec.ts
@@ -0,0 +1,23 @@
+import { Either, left, right } from './either'
+
+function doSomething(shouldSuccess: boolean): Either<string, string> {
+  if (shouldSuccess) {
+    return right('success')
+  } else {
+    return left('error')
+  }
+}
+
+test('success result', () => {
+  const successResult = doSomething(true)
+
+  expect(successResult.isRight()).toBe(true)
+  expect(successResult.isLeft()).toBe(false)
+})
+
+test('error result', () => {
+  const result = doSomething(false)
+
+  expect(result.isLeft()).toBe(true)
+  expect(result.isRight()).toBe(false)
+})
---
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,39 @@
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Get Question By Slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    )
+
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  test('Should be able to get a question by slug', async () => {
+    const newQuestion = makeQuestion({
+      slug: Slug.create('example-question'),
+    })
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    const result = await sut.execute({
+      slug: 'example-question',
+    })
+
+    expect(result.value).toMatchObject({
+      question: expect.objectContaining({
+        title: newQuestion.title,
+      }),
+    })
+  })
+})
